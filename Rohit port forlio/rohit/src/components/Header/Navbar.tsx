b56f17e4fc1a70962ac5186f8615c6ab9d2e80df
Navnoor Singh <navnoorsinghthind1354@gmail.com>
2024-06-18 11:25:27 +0530

    "Office work done"

---
+++ Rohit port forlio/rohit/src/components/Header/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="">
+        <div className=""></div>
+        <div className="">
+          <p className=""></p>
+          <p className=""></p>
+          <p className=""></p>
+          <p className=""></p>
+        </div>
+        <div className=""></div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
